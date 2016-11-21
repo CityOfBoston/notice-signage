5b62cd5e571c31d958780146c3ccc3b7f9514706
Matthew Crist <matthew.crist@boston.gov>
2016-11-21 15:57:00 -0500

    Remove the absolute positioning from the app container

--- src/App.vue
+++ src/App.vue
@@ -165,16 +165,13 @@ export default {
 }
 </script>
 
-<style scoped>
+<style>
 .app {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  display: flex;
   flex-direction: column;
+  display: flex;
+  height: 100%;
   max-height: 1080px;
+  overflow: hidden;
 }
 
 .header {
