6f5fa317ac73fba347b487789538afbbef11ee93
Matthew Crist <matthew.crist@boston.gov>
2016-11-28 13:59:42 -0500

    Update tests to pass

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -11,9 +11,8 @@ module.exports = {
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.hello')
-      .assert.containsText('h1', 'Welcome to Your Vue.js App')
-      .assert.elementCount('img', 1)
+      .assert.elementPresent('.countdown')
+      .assert.containsText('.view', 'View all public notices at boston.gov/public-notices')
       .end()
   }
 }
